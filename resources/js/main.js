2ce440462a06d54b81cce3eee1421ae4ecf9c9ea
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-07-18 18:03:09 -0500

    reveal on mobile functional

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,23 @@
 $(document).ready(() => {
 
+    const $software = $('#software');
+    const $computerArt = $('#computer-art');
+    const $design = $('#design');
+    const $data = $('#data');
+    const $business = $('#business');
+    const $marketing = $('#marketing');
+
+    const $expands = $('.expands');
+    const $expandc = $('.expandc');
+    const $expandde = $('.expandde');
+    const $expandda = $('.expandda');
+    const $expandb = $('.expandb');
+    const $expandm = $('.expandm');
+
     const $courseTitle = $('.course-titlem');
-    const $courseButton = $('.coursem');
-    const $courseInfo = $('.expand');
+
+    /*const $courseButton = $('.coursem');*/
+    /*const $courseInfo = $('.expand');
 
     $courseButton.on('click', () => {
         $courseInfo.show();
@@ -10,6 +25,30 @@ $(document).ready(() => {
 
     $courseTitle.on('click', () => {
         $courseInfo.hide();
+    });*/
+
+    $software.on('click', () => {
+        $expands.show();
+    });
+
+    $computerArt.on('click', () => {
+        $expandc.show();
+    });
+
+    $design.on('click', () => {
+        $expandde.show();
+    });
+
+    $data.on('click', () => {
+        $expandda.show();
+    });
+
+    $business.on('click', () => {
+        $expandb.show();
+    });
+
+    $marketing.on('click', () => {
+        $expandm.show();
     });
 
 
