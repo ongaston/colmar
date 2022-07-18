908c3c836e06da57b04b30a2d91de9e65de2f4b1
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-07-18 16:02:15 -0500

    work on jQuert script to show/hide course on mobile view

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(() => {
 
-    
+    const $courseTitle = $('.course-titlem');
+    const $courseButton = $('.coursem');
+    const $courseInfo = $('.expand');
+
+    $courseButton.on('click', () => {
+        $courseInfo.show();
+    });
+
+    $courseTitle.on('click', () => {
+        $courseInfo.hide();
+    });
+
 
 });
