b7044ea913362e10c0d6f98096c407a22a2f2f78
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-07-19 13:27:37 -0500

    make open courses on mobile collapse when another one is tapped

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,26 +2,56 @@ $(document).ready(() => {
 
     $('#software').on('click', () => {
         $('.expands').toggle();
+        $('.expandc').hide();
+        $('.expandde').hide();
+        $('.expandda').hide();
+        $('.expandb').hide();
+        $('.expandm').hide();
     })
 
     $('#computer-art').on('click', () => {
         $('.expandc').toggle();
+        $('.expands').hide();
+        $('.expandde').hide();
+        $('.expandda').hide();
+        $('.expandb').hide();
+        $('.expandm').hide();
     })
 
     $('#design').on('click', () => {
         $('.expandde').toggle();
+        $('.expandc').hide();
+        $('.expands').hide();
+        $('.expandda').hide();
+        $('.expandb').hide();
+        $('.expandm').hide();
     })
 
     $('#data').on('click', () => {
         $('.expandda').toggle();
+        $('.expandc').hide();
+        $('.expandde').hide();
+        $('.expands').hide();
+        $('.expandb').hide();
+        $('.expandm').hide();
     })
 
     $('#business').on('click', () => {
         $('.expandb').toggle();
+        $('.expandc').hide();
+        $('.expandde').hide();
+        $('.expandda').hide();
+        $('.expands').hide();
+        $('.expandm').hide();
     })
 
     $('#marketing').on('click', () => {
         $('.expandm').toggle();
+        $('.expandc').hide();
+        $('.expandde').hide();
+        $('.expandda').hide();
+        $('.expandb').hide();
+        $('.expands').hide();
     })
 
 
