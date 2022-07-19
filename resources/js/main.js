e7193a02deb5a5de2467000becaf5e842bddd4fa
Olivia Gaston <goldfishwithsalsa@gmail.com>
2022-07-18 19:19:22 -0500

    finish script for courses section

--- resources/js/main.js
+++ resources/js/main.js
@@ -27,28 +27,89 @@ $(document).ready(() => {
         $courseInfo.hide();
     });*/
 
+
+    let $softwarei = false;
+
     $software.on('click', () => {
-        $expands.show();
+        if ($softwarei===false) {
+            $expands.show();
+            $softwarei = !$softwarei;
+        }
+        else if ($softwarei===true) {
+            $expands.hide();
+            $softwarei =!$softwarei;
+        }
     });
+
+
+
+    let $computerArti = false;
 
     $computerArt.on('click', () => {
-        $expandc.show();
+        if ($computerArti===false){
+            $expandc.show();
+            $computerArti = !$computerArti;
+        }
+        else if ($computerArti===true) {
+            $expandc.hide();
+            $computerArti = !$computerArti;
+        }
     });
+
+
+    let $designi = false;
 
     $design.on('click', () => {
-        $expandde.show();
+        if ($designi===false) {
+            $expandde.show();
+            $designi = !$designi;
+        }
+        else if ($designi===true) {
+            $expandde.hide();
+            $designi = !$designi;
+        }
     });
+
+
+    let $datai = false;
 
     $data.on('click', () => {
-        $expandda.show();
+        if ($datai===false) {
+            $expandda.show();
+            $datai = !$datai;
+        }
+        else if ($datai===true) {
+            $expandda.hide();
+            $expandda = !$datai;
+        }
     });
+
+
+    let $businessi = false;
 
     $business.on('click', () => {
-        $expandb.show();
+        if ($businessi===false) {
+            $expandb.show();
+            $businessi = !$businessi;
+        }
+        else if ($businessi===true) {
+            $expandb.hide();
+            $businessi = !$businessi;
+        }
     });
 
+
+    let $marketingi = false;
+
     $marketing.on('click', () => {
-        $expandm.show();
+        if ($marketingi===false) {
+            $expandm.show();
+            $marketingi = !$marketingi;
+        }
+        else if ($marketingi===true) {
+            $expandm.hide();
+            $marketingi = !$marketingi;
+        }
     });
 
 
